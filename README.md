1b392c588488d0dde63e4cca16029ca9a0e93383
Deepeshgiri <dr.deepeshgiri@gmail.com>
2024-04-15 16:51:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# heartbubble
+ 
---
+++ script.js
@@ -0,0 +1,18 @@
+const bodyElement = document.querySelector("body");
+
+bodyElement.addEventListener("mousemove", (e) => {
+  const xPos = e.offsetX;
+  const yPos = e.offsetY;
+
+  const heartElement = document.createElement("span");
+  heartElement.style.left = xPos + "px";
+  heartElement.style.top = yPos + "px";
+  const size = Math.random() * 150;
+  heartElement.style.width = size + "px";
+  heartElement.style.height = size + "px";
+
+  bodyElement.appendChild(heartElement);
+  setTimeout(() => {
+    heartElement.remove();
+  }, 1000);
+});
